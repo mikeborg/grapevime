b745dea316c403385a0737e3b5418c4bb7cb7219
Mike Borgholthaus <mikeborg@gmail.com>
2013-08-22 22:22:33 -0600

    init

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,23 @@
+class SearchController < ApplicationController
+  def index
+    
+    respond_to do |format|
+      
+      conversations = []
+      (1..20).each do |conversation_index|
+        conversation = []
+        previous_id = nil
+        (1..Random.rand(15)).each do |comment_index|
+          id = Random.rand(999999999)
+          comment = { :id => id, :comment_id => previous_id, :content => "This is comment ##{comment_index}." }
+          conversation << { :comment => comment }
+          previous_id = id
+        end
+        conversations << { :conversation => conversation }
+      end
+      
+      response = { :conversations => conversations }
+      format.json { render :json => response }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grapevime::Application.config.secret_key_base = '9371100ad6cbbaa4e1c9927387833365e575894b0b00e62d5e395413fdf622c6b39b71317bf8ef149000527124ee54222dc5d6e3b8bb9e108db73c6dcf9c5eb7'
