d5b0a513ae78a8c3ed4d2af3db8a2d5536e30d76
Mike Borgholthaus <mikeborg@gmail.com>
2013-09-16 12:45:37 -0600

    fixed controller to accept message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -98,6 +98,6 @@ class CommentsController < ApplicationController
     end
     
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:message)
     end
 end
